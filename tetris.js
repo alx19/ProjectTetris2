f46fd99a49e1dfb797136c62ed853c6f9292f0bb
Alx19 <32356054+Alx19@users.noreply.github.com>
2018-02-11 14:18:03 +0300

    Update tetris.js

--- tetris.js
+++ tetris.js
@@ -1,6 +1,9 @@
 //главый файл. здесь мы создаем экземпляры объекта Game
 
 //выбираем опции для тетрисов
+//colourOnly - режим игры с 3 цветами. блоки сокращаются только когда ряд одного цвета
+//altControl - альтернативное управление(wasd)
+//djenMode - после падения блок присутствует, но не окрашивается, хардкорный режим
 var game1opt = {
     colourOnly: false,
     altControl: false,
@@ -11,6 +14,7 @@ var game2opt = {
     altControl: true,
     djenMode: false,
 }
+//создаем два тетриса
 var game1 = new Game('myCanvas','leaderBoard', game1opt,'scoreTetris1');
 var game2 = new Game('secondTetris','leaderBoard2', game2opt,'scoreTetris2')
 //обновления анимаций и диспетчера событий
